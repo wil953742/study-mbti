d22ec11ac670a2db8645e67b590e72f3d831ab30
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-12-19 23:12:11 +0900

    style: Observer.ts TS compile Error 1차 해결

--- src/core/observer.ts
+++ src/core/observer.ts
@@ -2,25 +2,33 @@ interface Fn {
   (): void;
 }
 
-let currentFn: Fn = null;
+interface ObserverMap<T> {
+  [key: string]: T;
+}
+
+interface Object {
+  [key: string]: any;
+}
+
+let currentFn: Fn = () => {};
 
 export const observe = (fn: Fn): void => {
   currentFn = fn;
-  fn();
-  currentFn = null;
+  fn(); // rendering => state.isToggleOn => false
+  currentFn = () => {};
 };
 
 export const observable = (obj: object): object => {
-  const observerMap = {};
+  const observerMap: ObserverMap<Set<Fn>> = {};
 
   const state: object = new Proxy(obj, {
-    get: (obj: object, prop) => {
-      observerMap[prop] = observerMap[prop] || new Set();
+    get: (obj: Object, prop: string) => {
+      observerMap[prop] = observerMap[prop] || new Set<Fn>();
       if (currentFn) observerMap[prop].add(currentFn);
       return obj[prop];
     },
 
-    set: (obj: object, prop, value): boolean => {
+    set: (obj: Object, prop: string, value: any): boolean => {
       if (obj[prop] === value) return true; // 같은 값으로 업데이트하는 경우 함수가 재실행 되는 것을 방지
       if (JSON.stringify(obj[prop]) === JSON.stringify(value)) return true; // 같은 값으로 업데이트하는 경우 함수가 재실행 되는 것을 방지
       obj[prop] = value;
