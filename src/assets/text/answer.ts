782fbcc1c22259b43bf63f4db1b529a484aaaad1
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-12-27 01:50:50 +0900

    file: 데이터, 이미지 파일 업로드

---
+++ src/assets/text/answer.ts
@@ -0,0 +1,50 @@
+export const ANSWER: Answer = {
+  1: {
+    selectA: 'E',
+    selectB: 'I',
+  },
+  2: {
+    selectA: 'S',
+    selectB: 'N',
+  },
+  3: {
+    selectA: 'T',
+    selectB: 'F',
+  },
+  4: {
+    selectA: 'J',
+    selectB: 'P',
+  },
+  5: {
+    selectA: 'E',
+    selectB: 'I',
+  },
+  6: {
+    selectA: 'S',
+    selectB: 'N',
+  },
+  7: {
+    selectA: 'T',
+    selectB: 'F',
+  },
+  8: {
+    selectA: 'P',
+    selectB: 'J',
+  },
+  9: {
+    selectA: 'E',
+    selectB: 'I',
+  },
+  10: {
+    selectA: 'S',
+    selectB: 'N',
+  },
+  11: {
+    selectA: 'F',
+    selectB: 'T',
+  },
+  12: {
+    selectA: 'J',
+    selectB: 'P',
+  },
+};
