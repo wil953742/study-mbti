8f0bc3a59de9256819e06f9a7eb88b61eb689ca1
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2022-01-26 23:24:58 +0900

    chore: 제목 변경, css 설정 캡쳐

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -16,7 +16,7 @@ export default class Home extends Component {
       </header>
       <section class="home-title-section">
         <p class="home-main-title-weak">무선생과 함께하는</p>
-        <p class="home-main-title-bold">공부 mbti 테스트</p>      
+        <p class="home-main-title-bold">공부 유형 테스트</p>      
       </section>
       <section class="home-img-section">
         <p class="home-sub-title">나에게 꼭 맞는 공부 방법은?</p>
