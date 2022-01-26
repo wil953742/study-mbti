736c17406ead4a24898568b7c8421e6e74996c6d
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2022-01-26 10:50:48 +0900

    feat: omr 카드 이미지 추가

--- src/components/progress/index.ts
+++ src/components/progress/index.ts
@@ -2,11 +2,13 @@ import Component from '@core/component';
 import './style.scss';
 
 import { $ } from '@util/query-selector';
+import omr from '@assets/images/ic-omr.svg';
 
 export default class Progress extends Component {
   template(): string {
     const currentPage: number = (this.props.value as number) + 1;
     return `
+        <img src=${omr} alt="omr-image" />
         <section class="progress-bar-section">
           <div class="background-bar"></div>
           <div class="progress-bar"></div>
