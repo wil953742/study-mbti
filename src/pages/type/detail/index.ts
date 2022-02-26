13d6a0a3c4100045872fa32d416a75430e5e0aeb
김웅일 <wil953742@naver.com>
2022-02-26 23:46:59 +0900

    feat: type-detail 수정

--- src/pages/type/detail/index.ts
+++ src/pages/type/detail/index.ts
@@ -2,21 +2,68 @@ import Component from '@core/component';
 import './style.scss';
 import { $ } from '@util/query-selector';
 import { RESULT } from '@assets/text/result';
-import logoImg from '@assets/images/logo.png';
-import resultImg from '@assets/images/temp-result.png';
+import icShare from '@assets/images/ic-share.svg';
+import icAgain from '@assets/images/ic-again.svg';
+import resultImg from '@assets/images/img-sample.png';
 
 export default class Detail extends Component {
   template(): string {
-    const { title, hashtag, strategy } = RESULT[this.props.value as string];
+    const { subTitle, mainTitle, hashtag1, hashtag2, hashtag3, strategy } =
+      RESULT[this.props.value as string];
 
     return `
-        <header class="home-logo">
-          <img src="${logoImg}" alt="logo" class="logo-img"/>
-        </header>
-        <h1 class="result-title">${title}</h1>
-        <img src="${resultImg} alt="result-img" class="result-img" />
-        <p class="result-hashtag">${hashtag}</p>
-        <p class="result-strategy">${strategy}</p>
+        <section class="result-header">
+          <h3>${subTitle}</h3>
+          <h1>${mainTitle}</h1>
+        </section>
+        <section class="result-hashtag">
+          <p>${hashtag1}</p>
+          <p>${hashtag2}</p>
+          <p>${hashtag3}</p>
+        </section>
+        <section class="result-character">
+          <img src="${resultImg} alt="result-img" />
+        </section>
+        <section class="result-graph"> 
+          <svg></svg>
+        </section>
+        <section class="result-overview">
+          <section>
+            <div class="double-line"></div>
+            <p>유형 특징</p>
+            <div class="double-line"></div>
+          </section>
+          <div>
+            - 유형특징 1
+          </div>
+          <div>
+            - 유형특징 2
+          </div>
+        </section>
+        <section class="result-recommend">
+          <section>
+            <div class="double-line"></div>
+            <p>이런 유형을 위한 추천</p>
+            <div class="double-line"></div>
+          </section>
+          <div>
+          - 유형추천 1
+          </div>
+          <div>
+          - 유형추천 2
+          </div>
+        </section>
+        <section class="result-btn">
+          <button>다른 결과 유형 보러가기</button>
+          <button>
+            <img src="${icShare}" alt="share-icon"/>
+            테스트 결과 공유하기
+          </button>
+          <button>
+            <img src="${icAgain}" alt="again-icon"/>
+            테스트 다시하기
+          </button>
+        </section>
       `;
   }
 }
